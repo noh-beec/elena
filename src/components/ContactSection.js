cde2457d6af46309c33dc75a735c52ae00d3dd19
carlos <nohbeec07@gmail.com>
2020-04-24 23:00:34 -0500

    modal para productos y ajustes de react

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,92 +1,82 @@
 import React from 'react';
-import './FaqSection.css';
-import Banner from './img/elenafondo1.png';
+import './ContactSection.css';
+import Banner from './img/elenafondo2.png';
 
-const FaqSection = (props) => {
+const ContactSection = (props) => {
+    
     return (
-       <>
-         <div className="banner-products" style={{backgroundImage: `url(${Banner})`}}>
-            <h2 className="animated infinite bounce delay-2s">Preguntas Frecuentes</h2>
+      <>
+       
+       <div className="banner-products" style={{backgroundImage: `url(${Banner})`}}>
+            <h2 className="animated infinite bounce delay-2s">Contacto</h2>
         </div>
-       <div id="faq" className="faq" >
-           <h3><b>Sobre nosotros</b></h3>
-           <ul>
-               <li><b>¿Quiénes somos?</b></li>
-               <br/>
-                Somos una tienda dedicada a la venta de platería y artesaías 
-                típicas Méxicanas.
-               
-               <br/>
-               <br/>
-               <li><b>¿En dónde nos ubicamos? </b></li>
-               <br/>
-               En el mercado de artesanías "La ciudadela" en la CDMX, local 206.
-              
-               <br/>
-               <br/>
-               <li><b>¿De dónde vienen los productos? </b></li>
-               <br/>
-               Adquirimos nuestros productos de diversos estados de la republica,
-               principalmente de Taxco, Guerrero.
-              
-               <br/>
-               <br/>
-               <li><b>¿Cómo se han fabricado tus productos?</b></li>
-               <br/>
-               Nuestros productos son fabricados por artesanos 100% méxicanos.
-               
-               <br/>
-               <br/>
-               <li><b>¿Utilizan materiales de calidad?</b> </li>
-               <br/>
-               Los artesanos fabrican los productos con los mejores materiales
-               de calidad y amigables con el medio ambiente.
-              
-           </ul>
-           <br/>
-           <br/>
-           <br/>
-        </div>
-        <div className="faq">
-            <h3><b>Nuestros productos</b></h3>
-           <ul>
-               <li><b>¿Cómo adquirir nuestros productos?</b></li>
-               <br/>
-               Puede contactarse con nosotros desde nuestro sitio web o 
-               en los locales 206 
-               Balderas S/N, Colonia Centro, Cuauhtémoc, 
-               06040, CDMX a una cuadra del metro Balderas.
-               
-               <br/>
-               <br/>
-               <li><b>¿Hacen envíos?</b></li>
-               <br/>
-               Puedes contactarte con nosotros para cotizar cualquier tipo de pedido.
-               <br/>
-               <br/>
-               <li><b>¿Qué hago si quiero devolver un producto?</b></li>
-               <br/>
-               Si no te encuentras satisfecho con nuestro producto ponte en 
-               contacto con nosotros. 
-               
-               <br/>
-               <br/>
-               <li><b>¿Qué formas de pago están disponibles?</b> </li>
-               <br/>
-               Pago en efectivo y tarjeta en nuestros locales, de manera digital contamos 
-               con sistema de paypal y transferencia bancaria.
-              
-               <br/>
-               <br/>
-               <li><b>¿Cómo puedo ponerme en contacto con ustedes?</b></li>
-               <br/>
-               En nuestro sitio se encuentra toda nuestra información de contacto en la 
-               sección correspondiente.
-               
-           </ul>
-       </div>
-       </>
+        <div id="contact"></div>
+      <div  id="contatti">
+               <div className="container mt-5">
+
+                   <div className="row" style={{"height":"550px;"}}>
+                       <div className="col-md-6 maps">
+                           <iframe
+                                title={'mapa'}
+                               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15050.287749421748!2d-99.1491503!3d19.4308928!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x10fdffa52b4ee0!2sMercado%20De%20Artesan%C3%ADas%20La%20Ciudadela!5e0!3m2!1ses-419!2smx!4v1587080715215!5m2!1ses-419!2smx"
+                               frameBorder="0" style={{"border":"0"}} allowFullScreen></iframe>
+                       </div>
+                       <div className="col-md-6">
+                           <h2 className="text-uppercase mt-3 font-weight-bold text-white">Contacto</h2>
+                           <form action="">
+                               <div className="row">
+                                   <div className="col-lg-6">
+                                       <div className="form-group">
+                                           <input type="text" className="form-control mt-2" placeholder="Nombre"
+                                                  required />
+                                       </div>
+                                   </div>
+                                  
+                                   <div className="col-lg-6">
+                                       <div className="form-group">
+                                           <input type="email" className="form-control mt-2" placeholder="Email"
+                                                  required />
+                                       </div>
+                                   </div>
+                                   <div className="col-lg-6">
+                                       <div className="form-group">
+                                           <input type="number" className="form-control mt-2" placeholder="Teléfono"
+                                                  required />
+                                       </div>
+                                   </div>
+                                   <div className="col-12">
+                                       <div className="form-group">
+                                           <textarea className="form-control" id="exampleFormControlTextarea1"
+                                                     placeholder="Comentarios" rows="3" required></textarea>
+                                       </div>
+                                   </div>
+                                   <div className="col-12">
+                                       <div className="form-group">
+                                           <div className="form-check">
+                                               <input className="form-check-input" type="checkbox" value=""
+                                                      id="invalidCheck2" required />
+                                                   <label className="form-check-label" htmlFor="invalidCheck2">
+                                                       Acepto las condiciones
+                                                   </label>
+                                           </div>
+                                       </div>
+                                   </div>
+                                   <div className="col-12">
+                                       <button className="btn" type="submit">Enviar</button>
+                                   </div>
+                               </div>
+                           </form>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+      </>
     )
 }
 
-export default FaqSection
+
+
+
+
+export default ContactSection
